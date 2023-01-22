d983d3c6345e40b890384fbd9923e335c0ac0ea9
poornaSavindi <89610997+poornaSavindi@users.noreply.github.com>
2023-01-22 19:54:36 +0530

    Add team description

--- src/Views/HomePage/MeetTeam/MeetTeam.jsx
+++ src/Views/HomePage/MeetTeam/MeetTeam.jsx
@@ -1,32 +1,19 @@
 import React from 'react'
 import styles from './meetteam.module.css'
-import Member from './Member/memberComponent'
 
 const MeetTeam = () => {
   return (
     <div>
       <div className={`${styles['team']}`}>
-        <div className={`${styles['team-title']}`}>
-          <span style={{color:'#102c42',borderTop:'3px solid #0984e3'}}>Meet </span><span style={{color:'#ffffff'}}>the Team</span>
-        </div>
-        <div className={`${styles['container']}`}>
-          {/* <div className={`${styles['group-photo']}`}>
-            <img src={require('../../../assests/images/team.jpeg')} />
+        <div className={`${styles['team-overlay']}`}>
+          <div className={`${styles['team-title']}`}>
+            Our Team
           </div>
-          <div className={`${styles['captain-contaier']}`}>
-              <div className={`${styles['member']}`}>
-                <Member img={'Captain_female.JPG'} name={'Nethmi Jayakody'} tag={'Captain'}/>
-              </div>
-              <div className={`${styles['member']}`}>
-              <Member img={'ViceCaptain_female.jpg'} name={'Dulja Bamunusinghe'} tag={'Vice Captain'}/>
-              </div>
-              <div className={`${styles['member']}`}>
-              <Member img={'Captain_male.jpg'} name={'Nadun Gunawardana'} tag={'Captain'}/>
-              </div>
-              <div className={`${styles['member']}`}>
-              <Member img={'ViceCaptain_male.jpg'} name={'Vinul Fernando'} tag={'Vice Captain'}/>
-              </div>
-          </div> */}
+          <div className={`${styles['team-description']}`}>
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Doloremque tempora perferendis magnam repellat 
+            adipisci similique facilis repudiandae. Consequuntur, eos debitis, harum facilis esse 
+            eaque quos praesentium autem, vero hic dolor.
+          </div>
         </div>
       </div>
     </div>
