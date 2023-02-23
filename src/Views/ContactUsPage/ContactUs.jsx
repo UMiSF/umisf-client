46943473c67cb57b15e10c2e2fcef89702da5023
poornaSavindi <prnsavindi11@gmail.com>
2023-02-23 15:36:09 +0530

    Fix errors

--- src/Views/ContactUsPage/ContactUs.jsx
+++ src/Views/ContactUsPage/ContactUs.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect } from 'react'
 import styles from './contactus.module.css'
 import Footer from '../HomePage/Footer/footer'
-import Header from '../HomePage/Header/Header'
+import Header from '../HeaderPage/HeaderPage'
 import Member from './Member/memberComponent'
 
 function ContactUs() {
@@ -13,49 +13,9 @@ function ContactUs() {
   return (
     <div>
       <div className={`${styles['headerDiv']}`}>
-        <div className={`${styles['navBarList']}`}>
-          <img src={require("../../assests/images/umisf_logo.png")} alt={'logo'}></img>
-          <ul>
-          <li className={`${styles['navBarItem']}`}>
-                  <a href="/">Home</a>
-                  </li>
-            <li className={`${styles['navBarItem']}`}>
-              <a href="#">About</a>
-            </li>
-            <li className={`${styles['navBarItem']}`}>
-              <a href="/contact-us">Contact Us</a>
-            </li>
-            <li className={`${styles['navBarItem']}`}>
-              <a href="#">Gallery</a>
-            </li>
-            <li className={`${styles['navBarItem']}`}>
-              <a href="#">Register</a>
-            </li>
-          </ul>
-        </div>
-        {/* minimized side nav bar */}
-        <i  id='toggle-btn' className={`${styles['toggle-button']} bx bx-menu`} onClick={loadSideBar}></i>
-        <img id='side-img' className={`${styles['side-img']}`} src={require("../../assests/images/umisf_logo.png")} alt={'logo'}></img>
-        <div id='navSideBar' className={`${styles['navSideBar']}`}>
-            <ul>
-            <li className={`${styles['navBarItem']}`}>
-                  <a href="/">Home</a>
-                  </li>
-                <li className={`${styles['navBarItem']}`}>
-                <a href="#">About</a>
-                </li>
-                <li className={`${styles['navBarItem']}`}>
-                <a href="#">Contact Us</a>
-                </li>
-                <li className={`${styles['navBarItem']}`}>
-                <a href="#">Gallery</a>
-                </li>
-                <li className={`${styles['navBarItem']}`}>
-                <a href="#">Register</a>
-                </li>
-            </ul>
-        </div>
-        {/* ---- */}
+        
+        <Header/>
+        
         <div className={`${styles['UMiSF-container']}`}>
           <h1>Contact Us</h1>
         </div>
