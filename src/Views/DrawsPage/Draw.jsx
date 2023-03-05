5cfaf88eb0c14e712c5f661c0bc37d4a9343feb7
Kumuthu Athukorala <kumuthua@gmail.com>
2023-03-05 15:07:57 +0530

    draws page responsiveness

--- src/Views/DrawsPage/Draw.jsx
+++ src/Views/DrawsPage/Draw.jsx
@@ -1,89 +1,237 @@
-import { Button, Grid, Typography } from "@mui/material";
+import { Button, Grid, Typography, Box } from "@mui/material";
 import { useState } from "react";
 import { Bracket, Seed, SeedItem, SeedTeam } from "react-brackets";
+import { createTheme } from "@mui/material/styles";
+import { color, display } from "@mui/system";
 
-export default function Draw(props){
+const theme = createTheme({
+  breakpoints: {
+    values: {
+      xs: 0, // extra small screen sizes
+      sm: 600, // small screen sizes
+      md: 960, // medium screen sizes
+      lg: 1280, // large screen sizes
+      xl: 1920, // extra large screen sizes
+    },
+  },
+});
 
-  const {rounds} = props;
-    const [titleIndex,setTitleIndex] = useState(0);
+export default function Draw(props) {
+  const { rounds } = props;
+  const [titleIndex, setTitleIndex] = useState(0);
 
-  const CustomSeed = ({seed, breakpoint, roundIndex, seedIndex}) => {
+  const CustomSeed = ({ seed, breakpoint, roundIndex, seedIndex }) => {
     // breakpoint passed to Bracket component
     // to check if mobile view is triggered or not
-  
+
     // mobileBreakpoint is required to be passed down to a seed
     return (
-      <Seed mobileBreakpoint={breakpoint} style={{ fontSize: 12 }}>
-        <SeedItem>
-          <div>
-            <SeedTeam style={{backgroundColor: seed.winner === seed.teams[0]?.name? '#495283':'#1a1d2e'  }}>
-              
-              {seed.winner? (
+      //TODO - check seed and seedItem
+      <Seed
+        mobileBreakpoint={breakpoint}
+        style={{ fontSize: 12, width: "100%" }}
+      >
+        <SeedItem style={{ backgroundColor: "#040924", width: "100%" }}>
+          <Box
+            sx={{
+              [theme.breakpoints.up("sm")]: {
+                width: "100%",
+              },
+              [theme.breakpoints.down("sm")]: {
+                width: "100vw",
+              },
+            }}
+          >
+            <SeedTeam
+              style={{
+                width: "100%",
+                backgroundColor:
+                  seed.winner === seed.teams[0]?.name ? "#495283" : "#1a1d2e",
+              }}
+            >
+              {seed.winner ? (
                 <Grid container>
-                  <Grid item container sx={{width: '55%' , borderRightStyle: 'solid', borderColor: ' #adb3d1'}}>
-                    <Typography sx={{fontSize: '11px'}}>
-                      {seed.teams[0]?.name || 'NO TEAM '}
+                  <Grid
+                    item
+                    container
+                    sx={{
+                      [theme.breakpoints.up("sm")]: {
+                        width: "4.5vw",
+                      },
+                      [theme.breakpoints.down("sm")]: {
+                        width: "40vw",
+                      },
+                      borderRightStyle: "solid",
+                      borderColor: "#adb3d1",
+                    }}
+                  >
+                    <Typography sx={{ fontSize: "11px" }}>
+                      {seed.teams[0]?.name || "NO TEAM "}
                     </Typography>
                   </Grid>
-                  <Grid item container sx={{width: '15%',display:'flex', justifyContent:'center',borderRightStyle: 'solid', borderColor: ' #adb3d1'}}>
-                    <Typography sx={{fontSize: '11px'}}>
+                  <Grid
+                    item
+                    container
+                    sx={{
+                      [theme.breakpoints.up("sm")]: {
+                        width: "2vw",
+                      },
+                      [theme.breakpoints.down("sm")]: {
+                        width: "10vw",
+                      },
+                      display: "flex",
+                      justifyContent: "center",
+                      borderRightStyle: "solid",
+                      borderColor: " #adb3d1",
+                    }}
+                  >
+                    <Typography sx={{ fontSize: "11px" }}>
                       {seed.score[0][0]}
-                    </Typography >
+                    </Typography>
                   </Grid>
-                  <Grid item container sx={{width: '15%',display:'flex', justifyContent:'center',borderRightStyle: 'solid', borderColor: ' #adb3d1'}}>
-                    <Typography sx={{fontSize: '11px'}}>
+                  <Grid
+                    item
+                    container
+                    sx={{
+                      [theme.breakpoints.up("sm")]: {
+                        width: "2vw",
+                      },
+                      [theme.breakpoints.down("sm")]: {
+                        width: "10vw",
+                      },
+                      display: "flex",
+                      justifyContent: "center",
+                      borderRightStyle: "solid",
+                      borderColor: " #adb3d1",
+                    }}
+                  >
+                    <Typography sx={{ fontSize: "11px" }}>
                       {seed.score[0][1]}
-                    </Typography >
+                    </Typography>
                   </Grid>
-                  <Grid item container sx={{width: '15%',display:'flex', justifyContent:'center'}}>
-                    <Typography sx={{fontSize: '11px'}}>
+                  <Grid
+                    item
+                    container
+                    sx={{
+                      [theme.breakpoints.up("sm")]: {
+                        width: "2vw",
+                      },
+                      [theme.breakpoints.down("sm")]: {
+                        width: "10vw",
+                      },
+                      display: "flex",
+                      justifyContent: "center",
+                    }}
+                  >
+                    <Typography sx={{ fontSize: "11px" }}>
                       {seed.score[0][2]}
-                    </Typography >
+                    </Typography>
                   </Grid>
                 </Grid>
-              ):(
-                <Grid item container sx={{width: '100%' }}>
-                    <Typography sx={{fontSize: '11px'}}>
-                      {seed.teams[0]?.name || 'NO TEAM '}
-                    </Typography>
-                  </Grid>
+              ) : (
+                <Grid item container sx={{ width: "100%" }}>
+                  <Typography sx={{ fontSize: "11px" }}>
+                    {seed.teams[0]?.name || "NO TEAM "}
+                  </Typography>
+                </Grid>
               )}
             </SeedTeam>
 
-            <SeedTeam style={{backgroundColor: seed.winner === seed.teams[1]?.name? '#495283':'#1a1d2e'  }}>
-              
-              {seed.winner? (
+            <SeedTeam
+              style={{
+                width: "100%",
+                backgroundColor:
+                  seed.winner === seed.teams[1]?.name ? "#495283" : "#1a1d2e",
+              }}
+            >
+              {seed.winner ? (
                 <Grid container>
-                  <Grid item container sx={{width: '55%' , borderRightStyle: 'solid', borderColor: ' #adb3d1'}}>
-                    <Typography sx={{fontSize: '11px'}}>
-                      {seed.teams[1]?.name || 'NO TEAM '}
+                  <Grid
+                    item
+                    container
+                    sx={{
+                      [theme.breakpoints.up("sm")]: {
+                        width: "4.5vw",
+                      },
+                      [theme.breakpoints.down("sm")]: {
+                        width: "40vw",
+                      },
+                      borderRightStyle: "solid",
+                      borderColor: " #adb3d1",
+                    }}
+                  >
+                    <Typography sx={{ fontSize: "11px" }}>
+                      {seed.teams[1]?.name || "NO TEAM "}
                     </Typography>
                   </Grid>
-                  <Grid item container sx={{width: '15%',display:'flex', justifyContent:'center',borderRightStyle: 'solid', borderColor: ' #adb3d1'}}>
-                    <Typography sx={{fontSize: '11px'}}>
-                      {seed.score[1][0] }
+                  <Grid
+                    item
+                    container
+                    sx={{
+                      [theme.breakpoints.up("sm")]: {
+                        width: "2vw",
+                      },
+                      [theme.breakpoints.down("sm")]: {
+                        width: "10vw",
+                      },
+                      display: "flex",
+                      justifyContent: "center",
+                      borderRightStyle: "solid",
+                      borderColor: " #adb3d1",
+                    }}
+                  >
+                    <Typography sx={{ fontSize: "11px" }}>
+                      {seed.score[1][0]}
                     </Typography>
                   </Grid>
-                  <Grid item container sx={{width: '15%',display:'flex', justifyContent:'center',borderRightStyle: 'solid', borderColor: ' #adb3d1'}}>
-                    <Typography sx={{fontSize: '11px'}}>
-                      {seed.score[1][1] }
+                  <Grid
+                    item
+                    container
+                    sx={{
+                      [theme.breakpoints.up("sm")]: {
+                        width: "2vw",
+                      },
+                      [theme.breakpoints.down("sm")]: {
+                        width: "10vw",
+                      },
+                      display: "flex",
+                      justifyContent: "center",
+                      borderRightStyle: "solid",
+                      borderColor: " #adb3d1",
+                    }}
+                  >
+                    <Typography sx={{ fontSize: "11px" }}>
+                      {seed.score[1][1]}
                     </Typography>
                   </Grid>
-                  <Grid item container sx={{width: '15%',display:'flex', justifyContent:'center'}}>
-                    <Typography sx={{fontSize: '11px'}}>
-                      {seed.score[1][2] }
+                  <Grid
+                    item
+                    container
+                    sx={{
+                      [theme.breakpoints.up("sm")]: {
+                        width: "2vw",
+                      },
+                      [theme.breakpoints.down("sm")]: {
+                        width: "10vw",
+                      },
+                      display: "flex",
+                      justifyContent: "center",
+                    }}
+                  >
+                    <Typography sx={{ fontSize: "11px" }}>
+                      {seed.score[1][2]}
                     </Typography>
                   </Grid>
                 </Grid>
-              ):(
-                <Grid item container sx={{width: '100%' }}>
-                    <Typography sx={{fontSize: '11px'}}>
-                      {seed.teams[1]?.name || 'NO TEAM '}
-                    </Typography>
-                  </Grid>
+              ) : (
+                <Grid item container sx={{ width: "100%" }}>
+                  <Typography sx={{ fontSize: "11px" }}>
+                    {seed.teams[1]?.name || "NO TEAM "}
+                  </Typography>
+                </Grid>
               )}
             </SeedTeam>
-          </div>
+          </Box>
         </SeedItem>
       </Seed>
     );
@@ -91,30 +239,49 @@ export default function Draw(props){
 
   const titles = rounds.map((round) => round.title);
 
-  function handleTitleChange(index){
-    setTitleIndex(index)
+  function handleTitleChange(index) {
+    setTitleIndex(index);
   }
-    return(
-      <Grid container>
-
-        <Grid sx={{display: {md:"none", xs:''}}} item container xs={12} justifyContent='center' mt={1}>
-          {titles.map((title,index) => <Button sx={{backgroundColor: '#1a1d2e'}} variant={index === titleIndex ? "contained":"outlined" }key={index} onClick={(e) => handleTitleChange(index)}>{title}</Button>)}
-        </Grid>
-
-        <Grid item container xs={12} mt={1}>
-          <Bracket rounds={rounds} renderSeedComponent={CustomSeed}  swipeableProps={{
-          enableMouseEvents: true,
-          animateHeight: true,
-          index: titleIndex,
-          onChangeIndex: setTitleIndex
-
-        }} roundTitleComponent={(title, roundIndex) => {
-                return <div style={{ textAlign: 'center' }}>{title}</div>;
-            }} 
-            mobileBreakpoint={900}
-          />         
-        </Grid>
+  return (
+    <Box sx={{ width: "80%" }}>
+      <Grid
+        sx={{ display: { md: "none", xs: "" } }}
+        item
+        container
+        justifyContent="center"
+        mt={2}
+      >
+        {titles.map((title, index) => (
+          <Button
+            sx={{ backgroundColor: "#1a1d2e" }}
+            variant={index === titleIndex ? "contained" : "outlined"}
+            key={index}
+            onClick={(e) => handleTitleChange(index)}
+          >
+            {title}
+          </Button>
+        ))}
       </Grid>
-        
-    )
+      <Grid container xs={12} mt={3} sx={{ marginX: "4" }}>
+        <Bracket
+          rounds={rounds}
+          renderSeedComponent={CustomSeed}
+          swipeableProps={{
+            enableMouseEvents: true,
+            animateHeight: true,
+            index: titleIndex,
+            onChangeIndex: setTitleIndex,
+          }}
+          roundTitleComponent={(title, roundIndex) => {
+            return (
+              <Typography variant="body" color="white" align="left" bgcolor="back">
+                {title}
+              </Typography>
+            );
+          }}
+          mobileBreakpoint={900}
+        />
+      </Grid>
+    </Box>
+  );
 }
--- src/Views/DrawsPage/DrawsPage.jsx
+++ src/Views/DrawsPage/DrawsPage.jsx
@@ -1,83 +1,174 @@
-import { Card, Grid } from '@mui/material';
-import React from 'react';
-import Draw from './Draw';
+import {
+  Card,
+  Box,
+  Grid,
+  CardContent,
+  Button,
+  Snackbar,
+  Alert,
+  Typography,
+} from "@mui/material";
+import FileDownloadIcon from "@mui/icons-material/FileDownload";
+import React from "react";
+import Draw from "./Draw";
+import Styles from "./DrawsPage.module.css";
 
 const DrawsPage = () => {
+  const [open, setOpen] = React.useState(false);
+  const [successful, setSuccessful] = React.useState(true);
 
-    const rounds = [
+  const handleClick = () => {
+    setOpen(true);
+  };
+
+  const handleClose = (event, reason) => {
+    if (reason === "clickaway") {
+      return;
+    }
+    setSuccessful(false);
+    setOpen(false);
+  };
+
+  const rounds = [
+    {
+      title: "Round one",
+      seeds: [
         {
-          title: 'Round one',
-          seeds: [
-            {
-              id: 1,
-              date: new Date().toDateString(),
-              teams: [{ name: 'Team A' }, { name: 'Team B' }],
-              score: [[12,23,23],[23,5,21]],
-              winner: 'Team A'
-    
-            },
-            {
-              id: 2,
-              date: new Date().toDateString(),
-              teams: [{ name: 'Team C' }, { name: 'Team D' }],
-              score: [[15,23,13],[23,5,23]],
-              winner: 'Team D'
-            },
-            {
-              id: 3,
-              date: new Date().toDateString(),
-              teams: [{ name: 'Team E' }, { name: 'Team F' }],
-              score: [['','',''],['','','']]
-            },
-            {
-              id: 4,
-              date: new Date().toDateString(),
-              teams: [{ name: 'Team G' }, { name: 'Team H' }],
-              score: [['','',''],['','','']]
-            },
+          id: 1,
+          date: new Date().toDateString(),
+          teams: [{ name: "Team A" }, { name: "Team B" }],
+          score: [
+            [12, 23, 23],
+            [23, 5, 21],
+          ],
+          winner: "Team A",
+        },
+        {
+          id: 2,
+          date: new Date().toDateString(),
+          teams: [{ name: "Team C" }, { name: "Team D" }],
+          score: [
+            [15, 23, 13],
+            [23, 5, 23],
+          ],
+          winner: "Team C",
+        },
+        {
+          id: 3,
+          date: new Date().toDateString(),
+          teams: [{ name: "Team E" }, { name: "Team F" }],
+          score: [
+            ["", "", ""],
+            ["", "", ""],
           ],
         },
         {
-          title: 'Round two',
-          seeds: [
-            {
-              id: 5,
-              date: new Date().toDateString(),
-              teams: [{ name: 'Team A' }, { name: 'Team D' }],
-              score: [['','',''],['','','']]
-            },
-            {
-              id: 6,
-              date: new Date().toDateString(),
-              teams: [{ name: 'TBD' }, { name: 'TBD' }],
-              score: [['','',''],['','','']]
-            }
+          id: 4,
+          date: new Date().toDateString(),
+          teams: [{ name: "Team G" }, { name: "Team H" }],
+          score: [
+            ["", "", ""],
+            ["", "", ""],
+          ],
+        },
+      ],
+    },
+    {
+      title: "Round two",
+      seeds: [
+        {
+          id: 5,
+          date: new Date().toDateString(),
+          teams: [{ name: "Team A" }, { name: "Team D" }],
+          score: [
+            ["", "", ""],
+            ["", "", ""],
           ],
         },
         {
-          title: 'Round three',
-          seeds: [
-            {
-              id: 7,
-              date: new Date().toDateString(),
-              teams: [{ name: 'TBD' }, { name: 'TBD' }],
-              score: [['','',''],['','','']]
-            }
+          id: 6,
+          date: new Date().toDateString(),
+          teams: [{ name: "TBD" }, { name: "TBD" }],
+          score: [
+            ["", "", ""],
+            ["", "", ""],
           ],
-        }
-      ];
+        },
+      ],
+    },
+    {
+      title: "Round three",
+      seeds: [
+        {
+          id: 7,
+          date: new Date().toDateString(),
+          teams: [{ name: "TBD" }, { name: "TBD" }],
+          score: [
+            ["", "", ""],
+            ["", "", ""],
+          ],
+        },
+      ],
+    },
+  ];
 
-
-    return (
-        <Grid container>
-            <Grid item container m={2} justifyContent='center'>
-                <Card variant='elevation' elevation={3} sx={{ overflow: 'auto', display: 'flex'}}>
-                    <Draw rounds={rounds} />
-                </Card> 
-            </Grid>
+  return (
+    <div className={Styles["page"]}>
+      <Grid container>
+        <Grid item container m={2} justifyContent="center">
+          <div className={Styles["header"]}>Competition Draws</div>
         </Grid>
-        
-    );
+        <Grid item container justifyContent="center">
+          <div className={Styles["header-line-container"]}>
+            <div className={Styles["header-line"]}>School</div>
+            <div className={Styles["header-line"]}> {"|"}</div>
+            <div className={Styles["header-line"]}> Single</div>
+            <div className={Styles["header-line"]}> {"|"}</div>
+            <div className={Styles["header-line"]}> Under 19 </div>
+          </div>
+        </Grid>
+        <Grid item container justifyContent="center">
+          <Box
+            sx={{ display: "flex", justifyContent: "center", width: "100%" }}
+          >
+            <Draw rounds={rounds} />
+          </Box>
+        </Grid>
+        <Grid item container marginRight={10} justifyContent="right">
+          <Button
+            variant="outlined"
+            startIcon={<FileDownloadIcon />}
+            onClick={handleClick}
+          >
+            Download PDF
+          </Button>
+          <Snackbar open={open} autoHideDuration={6000} onClose={handleClose}>
+            {successful ? (
+              <Alert
+                onClose={handleClose}
+                severity="success"
+                sx={{ width: "100%" }}
+              >
+                <Typography sx={{ fontSize: "1vw" }}>
+                  Download successful!
+                </Typography>
+              </Alert>
+            ) : (
+              <Alert
+                severity="error"
+                onClose={handleClose}
+                sx={{ width: "100%" }}
+              >
+                <Typography sx={{ fontSize: "1vw" }}>
+                  Download failed!
+                </Typography>
+              </Alert>
+            )}
+          </Snackbar>
+        </Grid>
+      </Grid>
+    </div>
+  );
 };
 
 export default DrawsPage;
