20827b1d9048aa09d744bab3e61111f0e45f6545
poornaSavindi <prnsavindi11@gmail.com>
2023-03-18 13:51:31 +0530

    Get user type as props

--- src/Views/AdminHeaderPage/AdminHeader.jsx
+++ src/Views/AdminHeaderPage/AdminHeader.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import styles from "./adminHeader.module.css";
 
-const AdminHeader = () => {
+const AdminHeader = (props) => {
   return (
     <div>
       <div className={`${styles["header-container"]}`}>
@@ -12,7 +12,7 @@ const AdminHeader = () => {
           ></img>
         </div>
         <div className={`${styles["header-admin-profile"]}`}>
-          <div className={`${styles["header-admin"]}`}>ADMIN</div>
+          <div className={`${styles["header-admin"]}`}>{props.user_type}</div>
           <div className={`${styles["header-dropdown"]}`}>
             <img
               className={`${styles["header-drop-down-logo"]}`}
