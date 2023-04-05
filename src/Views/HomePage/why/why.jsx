05da3a038dd4654c9eb47dd3fd788e5eb0741fb7
Yasira Punsith <yasirapunsith@gmail.com>
2023-04-05 18:19:11 +0530

    add some content

--- src/Views/HomePage/why/why.jsx
+++ src/Views/HomePage/why/why.jsx
@@ -8,11 +8,13 @@ const Why = () => {
         ABOUT <p style={{ display: "inline-block", color: "#0984E3" }}>UMiSF</p>
       </div>
       <div className={`${styles["why-desc"]}`}>
-        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nemo voluptatum laudantium
-        molestias nihil quia. Vero incidunt mollitia vitae, sunt similique, ut ipsam quo natus
-        dignissimos architecto sed ducimus id quas? Lorem ipsum dolor sit amet, consectetur
-        adipisicing elit. Nemo voluptatum laudantium molestias nihil quia. Vero incidunt mollitia
-        vitae, sunt similique, ut ipsam quo natus dignissimos architecto sed ducimus id quas?
+        UMiSF is the annual badminton tournament organized by the badminton team
+        of the University of Moratuwa, in collaboration with the university's
+        division of Physical Education. We warmly welcome badminton enthusiasts
+        from across the island to come together in a spirit of friendly
+        competition and sportsmanship, to showcase their skills, with the
+        intention of facilitating connections among individuals who share a
+        passion for badminton.
       </div>
       <div className={`${styles["why-view-more"]}`}>
         <a href="/about" type="button">
