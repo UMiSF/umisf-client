561cce809b634f87ad451c8d9a17045298bdefaa
geemal <geemal1976@gmail.com>
2026-01-17 20:38:46 +0530

    Fix missing homepage images

--- src/Views/HomePage/HomePage.jsx
+++ src/Views/HomePage/HomePage.jsx
@@ -19,18 +19,18 @@ const HomePage = () => {
     "2024-01-19",
     "2024-02-15",
   ]);
-  const [teamPhoto, setTeamPhoto] = useState("Team 2025.jpg");
+  const [teamPhoto, setTeamPhoto] = useState("team-image-new.jpeg");
 
   const [gallery, setGallery] = useState([
-    "10.jpg",
-    "11.jpg",
-    "12.jpg",
-    "14.jpg",
-    "13.jpg",
-    "15.jpg",
-    "16.jpg",
-    "17.jpg",
-    "18.jpg",
+    "1.jpg",
+    "2.jpg",
+    "3.jpg",
+    "4.jpg",
+    "5.jpg",
+    "6.jpg",
+    "7.jpg",
+    "8.jpg",
+    "9.jpg",
   ]);
 
   const [sponsers, setSponsers] = useState([
